27fbe07a7b07948580c435f69218e9fb2eb429b5
Ganesh <j.ganesh26@gmail.com>
2017-01-05 10:31:42 +0530

    Testing

---
+++ app/src/main/java/map/com/testgit/Utils.java
@@ -0,0 +1,13 @@
+package map.com.testgit;
+
+/**
+ * Created by tringapps-admin on 5/1/17.
+ */
+
+public class Utils {
+
+    public static int add(int a, int b) {
+        return a+b;
+    }
+
+}
--- app/src/test/java/map/com/testgit/ExampleUnitTest.java
+++ app/src/test/java/map/com/testgit/ExampleUnitTest.java
@@ -12,6 +12,6 @@ import static org.junit.Assert.*;
 public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+        assertEquals(20, Utils.add(10, 10));
     }
 }
