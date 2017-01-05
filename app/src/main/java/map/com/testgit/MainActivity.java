ca75731cf3b1e041131487ec1fa018b106cc9eee
tringappsadmin <archana.h@tringapps.com>
2017-01-05 11:12:58 +0530

    lol comment3

--- app/src/main/java/map/com/testgit/MainActivity.java
+++ app/src/main/java/map/com/testgit/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
         //Test2
 // lol lol lol lol lol lol lol lol lol
 //        learnt git hub :P
+//        Archana's new comment
         Utils.add(10,10);
     }
 }
